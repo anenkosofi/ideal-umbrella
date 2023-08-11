06d9c3e8b95e695ca4d5be6d4804348d60283b03
Sofiia Anenko <anenko.sofiia@gmail.com>
2023-08-11 21:46:51 +0300

    fix: change position

--- src/components/WhoWeAre/index.tsx
+++ src/components/WhoWeAre/index.tsx
@@ -30,23 +30,21 @@ const WhoWeAre: FC = () => {
   return (
     <section className="who">
       <Container>
-        <div className="who__container">
-          <img src={field} alt="Field" className="who__pic" />
-          <div className="who__content">
-            <p className="who__pre-heading">Eco Friendly</p>
-            <h2 className="who__heading">
-              Econis is a Friendly
-              <br /> Organic Store
-            </h2>
-            <ul className="who__list">
-              {items.map(({ id, title, description }) => (
-                <li key={id} className="who__item">
-                  <h3>{title}</h3>
-                  <p>{description}</p>
-                </li>
-              ))}
-            </ul>
-          </div>
+        <img src={field} alt="Field" className="who__pic" />
+        <div className="who__content">
+          <p className="who__pre-heading">Eco Friendly</p>
+          <h2 className="who__heading">
+            Econis is a Friendly
+            <br /> Organic Store
+          </h2>
+          <ul className="who__list">
+            {items.map(({ id, title, description }) => (
+              <li key={id} className="who__item">
+                <h3>{title}</h3>
+                <p>{description}</p>
+              </li>
+            ))}
+          </ul>
         </div>
       </Container>
     </section>
