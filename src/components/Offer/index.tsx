d291e089336bc4525eef26277ca8756950612612
Sofiia Anenko <sofiiaa@oddity.com>
2023-08-13 14:45:36 +0300

    feat: add container

--- src/components/Offer/index.tsx
+++ src/components/Offer/index.tsx
@@ -9,9 +9,11 @@ const Offer: FC = () => {
     return (
         <section className="organic">
             <Container>
-                <p className="organic__pre-heading">Offer</p>
-                <h2 className="organic__heading">We Offer Organic For You</h2>
-                <ProductList />
+                <div className="organic__container">
+                    <p className="organic__pre-heading">Offer</p>
+                    <h2 className="organic__heading">We Offer Organic For You</h2>
+                    <ProductList />
+                </div>
             </Container>
         </section>
     )
